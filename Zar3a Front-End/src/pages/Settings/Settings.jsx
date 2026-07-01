597550cd33339f07c560efcd6ec444064029c31b
Mostafa Ali <moustafakhameis@gmail.com>
2026-07-01 21:25:43 +0300

    Fix syntax error in Settings

--- Zar3a Front-End/src/pages/Settings/Settings.jsx
+++ Zar3a Front-End/src/pages/Settings/Settings.jsx
@@ -397,6 +397,7 @@ const Settings = () => {
                           </button>
                         )}
                       </div>
+                    </div>
                   </div>
 
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
